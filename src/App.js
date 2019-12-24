0deedc6bfb6bd7580268e7ab818ca91f05aff2db
LuciferAngelos <x-2@mail.ru>
2019-12-25 00:23:27 +0300

    starting lessons

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header>
+        Simple HTML
+        <ul>
+          <li>
+            HTML
+          </li>
+          <li>
+            CSS
+          </li>
+          <li>
+            JS
+          </li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
